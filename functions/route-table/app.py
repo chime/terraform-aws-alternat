a7f834fa53c8dafb560e3da8051d33598cbc0d4a
Andre Mercer <andre.mercer@chime.com>
2022-08-30 13:31:09 -0700

    modified:   app.py 	modified:   requirements.txt

--- functions/route-table/app.py
+++ functions/route-table/app.py
@@ -5,7 +5,7 @@ import time
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-def lambda_handler(event, context):
+def handler(event, context):
     try:
         logger.info(json.dumps(event))
     except Exception as e:
