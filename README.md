4d2e8a4a4a8104ff693a88e1dd235ae74bfbc300
Andre Mercer <andre.mercer@chime.com>
2022-08-31 17:23:22 -0700

    Finish quick refactor 	modified:   ../../README.md 	modified:   app.py 	modified:   template.yaml

--- README.md
+++ README.md
@@ -21,3 +21,19 @@ Example:
 cd functions/route-table
 sam local invoke NATRouteTableFunction -e event.json
 ```
+
+Making actual calls to AWS for testing
+In on terminal 1
+```
+AWS_PROFILE=ChimeNonstable-Administrator aws sso login
+```
+In terminal 2
+```
+cd fuctions/route-tables
+AWS_PROFILE=ChimeNonstable-Administrator sam build && AWS_PROFILE=ChimeNonstable-Administrator sam local start-lambda #This will start up a docker container running locally
+```
+
+To invoke the function back in terminal 1
+```
+AWS_PROFILE=ChimeNonstable-Administrator aws lambda invoke --function-name "NATRouteTableFunction" --endpoint-url "http://127.0.0.1:3001" --region us-east-1 --cli-binary-format raw-in-base64-out --payload file://functions/route-table/event.json --no-verify-ssl out.txt
+```
--- functions/route-table/app.py
+++ functions/route-table/app.py
@@ -13,15 +13,13 @@ EC2_KEY = "EC2InstanceId"
 autoscaling = boto3.client("autoscaling")
 ec2 = boto3.client("ec2")
 
-def get_vpc_zone_identifier_and_azs(auto_scaling_group):
+def get_vpc_zone_identifier(auto_scaling_group):
     asg_objects = autoscaling.describe_auto_scaling_groups(AutoScalingGroupNames=[auto_scaling_group])
     asg = asg_objects["AutoScalingGroups"][0]
     logger.info("ASG: %s", asg)
-    availability_zones = asg["AvailabilityZones"]
     vpc_zone_identifier = asg["VPCZoneIdentifier"]
-    logger.info("AVAILABILITY_ZONES: %s", availability_zones)
     logger.info("VPC_ZONE_IDENTIFIER: %s", vpc_zone_identifier)
-    return availability_zones, vpc_zone_identifier
+    return vpc_zone_identifier
 
 def get_vpc_and_subnet_id(vpc_zone_identifier):
     subnets = ec2.describe_subnets(SubnetIds=[vpc_zone_identifier])
@@ -56,7 +54,6 @@ def describe_and_replace_route(subnet_id, nat_gateway_id):
     route_table = route_tables['RouteTables'][0]
     logger.info("ROUTE_TABLE: %s", route_table)
     response = ec2.replace_route(
-        DryRun=True,
         DestinationCidrBlock="0.0.0.0/0",
         NatGatewayId=nat_gateway_id,
         RouteTableId=route_table["RouteTableId"],
@@ -75,11 +72,10 @@ def handler(event, context):
                 logger.info("LIFECYLE_HOOK: %s", life_cycle_hook)
                 logger.info("AUTO_SCALING_GROUP: %s", auto_scaling_group)
                 logger.info("INSANCE_ID: %s", instance_id)
-                availability_zones, vpc_zone_identifier = get_vpc_zone_identifier_and_azs(auto_scaling_group)
+                vpc_zone_identifier = get_vpc_zone_identifier(auto_scaling_group)
                 vpc_id, subnet_id = get_vpc_and_subnet_id(vpc_zone_identifier)
                 nat_gateway_id = get_nat_gatway_id(vpc_id, subnet_id)
                 describe_and_replace_route(subnet_id, nat_gateway_id)
-
 
     except Exception as e:
         logging.error("Error: %s", str(e))
