f1c22c6a188d8367d77d27f2f581e2eb3ba3161e
Andre Mercer <andre.mercer@chime.com>
2022-09-07 16:28:42 -0700

    Add error handling 	modified:   ../../README.md 	modified:   app.py

--- README.md
+++ README.md
@@ -29,11 +29,11 @@ AWS_PROFILE=ChimeNonstable-Administrator aws sso login
 ```
 In terminal 2
 ```
-cd fuctions/replace-route
+cd functions/replace-route
 AWS_PROFILE=ChimeNonstable-Administrator sam build && AWS_PROFILE=ChimeNonstable-Administrator sam local start-lambda #This will start up a docker container running locally
 ```
 
 To invoke the function back in terminal 1
 ```
-AWS_PROFILE=ChimeNonstable-Administrator aws lambda invoke --function-name "NATRouteTableFunction" --endpoint-url "http://127.0.0.1:3001" --region us-east-1 --cli-binary-format raw-in-base64-out --payload file://functions/replace-route/event.json --no-verify-ssl out.txt
+AWS_PROFILE=ChimeNonstable-Administrator aws lambda invoke --function-name "NATRouteTableFunction" --endpoint-url "http://127.0.0.1:3001" --region us-east-1 --cli-binary-format raw-in-base64-out --payload file://functions/replace-route/sns-event.json --no-verify-ssl out.txt
 ```
--- functions/replace-route/app.py
+++ functions/replace-route/app.py
@@ -16,22 +16,28 @@ ec2 = boto3.client("ec2")
 
 def get_vpc_zone_identifier(auto_scaling_group):
     asg_objects = autoscaling.describe_auto_scaling_groups(AutoScalingGroupNames=[auto_scaling_group])
-    asg = asg_objects["AutoScalingGroups"][0]
-    logger.info("ASG: %s", asg)
-    vpc_zone_identifier = asg["VPCZoneIdentifier"]
-    logger.info("VPC_ZONE_IDENTIFIER: %s", vpc_zone_identifier)
-    return vpc_zone_identifier
+    if asg_objects["AutoScalingGroups"] and len(asg_objects["AutoScalingGroups"]) > 0:
+        asg = asg_objects["AutoScalingGroups"][0]
+        logger.info("ASG: %s", asg)
+        vpc_zone_identifier = asg["VPCZoneIdentifier"]
+        logger.info("VPC_ZONE_IDENTIFIER: %s", vpc_zone_identifier)
+        return vpc_zone_identifier, None
+    else:
+        return None, "Failed to describe autoscaling group data"
 
 def get_vpc_and_subnet_id(vpc_zone_identifier):
     subnets = ec2.describe_subnets(SubnetIds=[vpc_zone_identifier])
-    subnet = subnets["Subnets"][0]
-    subnet_id = subnet["SubnetId"]
-    logger.info("SUBNET_ID: %s", subnet_id)
-    vpc_id = subnet["VpcId"]
-    logger.info("VPC_ID: %s", vpc_id)
-    return vpc_id, subnet_id
+    if subnets["Subnets"] and len(subnets["Subnets"]) > 0:
+        subnet = subnets["Subnets"][0]
+        subnet_id = subnet["SubnetId"]
+        logger.info("SUBNET_ID: %s", subnet_id)
+        vpc_id = subnet["VpcId"]
+        logger.info("VPC_ID: %s", vpc_id)
+        return vpc_id, subnet_id, None
+    else:
+        return None, None, "Failed to describe subnet data"
 
-def get_nat_gatway_id(vpc_id, subnet_id):
+def get_nat_gateway_id(vpc_id, subnet_id):
     nat_gateways = ec2.describe_nat_gateways(
         Filters=[{ "Name": "vpc-id",
                     "Values": [vpc_id]
@@ -42,9 +48,12 @@ def get_nat_gatway_id(vpc_id, subnet_id):
                     ])
     logger.info("SUBNET ID: %s", subnet_id)
     logger.info("NAT GATEWAY: %s", nat_gateways)
-    nat_gateway_id = nat_gateways['NatGateways'][0]["NatGatewayId"]
-    logger.info("NAT_GATEWAY_ID %s", nat_gateway_id)
-    return nat_gateway_id
+    if nat_gateways['NatGateways'] and len(nat_gateways['NatGateways']) > 0:
+        nat_gateway_id = nat_gateways['NatGateways'][0]["NatGatewayId"]
+        logger.info("NAT_GATEWAY_ID %s", nat_gateway_id)
+        return nat_gateway_id, None
+    else:
+        return None, "Failed To describe nat gateways"
 
 def describe_and_replace_route(subnet_id, nat_gateway_id):
     route_tables = ec2.describe_route_tables(
@@ -52,20 +61,26 @@ def describe_and_replace_route(subnet_id, nat_gateway_id):
                     "Values": [subnet_id]
                     }]
     )
-    route_table = route_tables['RouteTables'][0]
-    logger.info("ROUTE_TABLE: %s", route_table)
+    if route_tables['RouteTables'] and len(route_tables['RouteTables']) > 0:
+        route_table = route_tables['RouteTables'][0]
+        logger.info("ROUTE_TABLE: %s", route_table)
+    else:
+        return None, "Failed to describe route tables"
+
     response = ec2.replace_route(
         DestinationCidrBlock="0.0.0.0/0",
         NatGatewayId=nat_gateway_id,
         RouteTableId=route_table["RouteTableId"],
     )
     logger.info("RESPONSE: %s", response)
+    if response["ResponseMetadata"] and response["ResponseMetadata"]['HTTPStatusCode'] == 200:
+        return response, None
+    else:
+        return None, "Failed to replace route"
 
 def handler(event, context):
     logger.info(json.dumps(event))
-    sys.exit()
     try:
-        #logger.info(json.dumps(event))
         for record in event["Records"]:
             message = json.loads(record["Sns"]["Message"])
             if LIFECYCLE_KEY in message and ASG_KEY in message:
@@ -75,10 +90,35 @@ def handler(event, context):
                 logger.info("LIFECYLE_HOOK: %s", life_cycle_hook)
                 logger.info("AUTO_SCALING_GROUP: %s", auto_scaling_group)
                 logger.info("INSANCE_ID: %s", instance_id)
-                vpc_zone_identifier = get_vpc_zone_identifier(auto_scaling_group)
-                vpc_id, subnet_id = get_vpc_and_subnet_id(vpc_zone_identifier)
-                nat_gateway_id = get_nat_gatway_id(vpc_id, subnet_id)
-                describe_and_replace_route(subnet_id, nat_gateway_id)
+                vpc_zone_identifier, err = get_vpc_zone_identifier(auto_scaling_group)
+                if err is not None:
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps(err)
+                    }
+                vpc_id, subnet_id, err = get_vpc_and_subnet_id(vpc_zone_identifier)
+                if err is not None:
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps(err)
+                    }
+                nat_gateway_id, err = get_nat_gateway_id(vpc_id, subnet_id)
+                if err is not None:
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps(err)
+                    }
+                response, err = describe_and_replace_route(subnet_id, nat_gateway_id)
+                if err is not None:
+                    return {
+                        'statusCode': 400,
+                        'body': json.dumps(err)
+                    }
+                else:
+                    return {
+                        'statusCode': 200,
+                        'body': json.dumps("Route replace succeeded")
+                    }
 
     except Exception as e:
         logging.error("Error: %s", str(e))
