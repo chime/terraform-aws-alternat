4e188fa17767944f3887529428760861601bc0c9
Nicolas Richard <nicolas.richard@chime.com>
2022-10-11 12:22:48 -0700

    Remove unused arg `context` from `handler` function

--- functions/replace-route/app.py
+++ functions/replace-route/app.py
@@ -282,7 +282,7 @@ def describe_and_replace_route(subnet_id, nat_gateway_id):
         raise error
 
 
-def handler(event, context):  # Parameter `context` is not used
+def handler(event):
 
     try:
         for record in event["Records"]:
