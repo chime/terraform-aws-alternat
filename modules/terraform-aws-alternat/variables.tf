23f41c3c52fa0ed3c0c4bc21173ce0c82747d56a
amirbitt <86658800+amirbitt@users.noreply.github.com>
2023-08-23 12:38:48 +0300

    Update variables.tf

--- modules/terraform-aws-alternat/variables.tf
+++ modules/terraform-aws-alternat/variables.tf
@@ -101,6 +101,7 @@ variable "key_name" {
   description = "The SSH KEY to use for the NAT instance."
   type        = string
   default     = ""
+}
 
 variable "nat_instance_block_devices" {
   description = "Optional custom EBS volume settings for the NAT instance."
