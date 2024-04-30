343f4b9541058c5fcaed9459afbf81747df9da73
amirbitt <86658800+amirbitt@users.noreply.github.com>
2024-04-30 12:26:51 +0300

    Update and rename alternat_oldprod.sh to alternat-eu-west-1a_oldprod.sh

--- scripts/alternat_oldprod.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-USERDATA_CONFIG_FILE="/etc/alternat.conf"
-echo eip_allocation_ids_csv=eipalloc-05bed52af1132c4e5,eipalloc-0f766b3862a50a161,eipalloc-0597cfcaeac4a5a8f >> "$USERDATA_CONFIG_FILE"
-echo route_table_ids_csv=	rtb-f1de2795 >> "$USERDATA_CONFIG_FILE"
-
-
-
