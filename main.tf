d499416e87e97aa216add14e881832af17a67134
Romain Forlot <romain.forlot@dailymotion.com>
2025-06-20 10:08:56 +0200

    chore: factorize output for aws_eips
    Signed-off-by: Romain Forlot <romain.forlot@dailymotion.com>

--- main.tf
+++ main.tf
@@ -36,6 +36,8 @@ locals {
   # var.nat_instance_eip_ids ignored if doesn't match AZ count
   reuse_nat_instance_eips = length(var.nat_instance_eip_ids) == length(var.vpc_az_maps)
   nat_instance_eip_ids    = local.reuse_nat_instance_eips ? var.nat_instance_eip_ids : aws_eip.nat_instance_eips[*].id
+  nat_instance_eips       = var.prevent_destroy_eips ? aws_eip.protected_nat_instance_eips : aws_eip.nat_instance_eips
+  nat_gateway_eips        = var.prevent_destroy_eips ? aws_eip.protected_nat_gateway_eips : aws_eip.nat_gateway_eips
 }
 
 
